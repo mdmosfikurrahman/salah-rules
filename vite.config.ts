25d9a2029ae9377717148a189e0e0dd6480942f3
Mosfik <mdmosfikurrahman.cse@gmail.com>
2025-10-28 23:55:02 +0600

    Remove base URL configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-    base: "/salah-rules/",
     server: {
         host: "::",
         port: 8080,
